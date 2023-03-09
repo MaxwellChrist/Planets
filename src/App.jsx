a63dc5d58cd023630412e438f929422ebe77cc66
MaxwellChrist <ribthulu@gmail.com>
2023-03-09 14:08:26 -0500

    added react router dom and added appropriate links for each

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
 
-  const [mobileview, setMobileView] = useState
+  const [mobileview, setMobileView] = useState(null)
 
   return (
     <div className="App">
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,28 @@
-import './navbar.css'
+import "./navbar.css";
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-heading">
-            <h2>The Planets</h2>
-        </div>
-        <div className="navbar-list">
+      <header>
+        <nav className="navbar">
+          <div className="navbar-heading">
+            <h2><Link to="/">The Planets</Link></h2>
+          </div>
+          <div className="navbar-list">
             <ul>
-                <li>Mercury</li>
-                <li>Venus</li>
-                <li>Earth</li>
-                <li>Mars</li>
-                <li>Jupiter</li>
-                <li>Saturn</li>
-                <li>Uranus</li>
-                <li>Neptune</li>
+              <li><Link to="/mercury">Mercury</Link></li>
+              <li><Link to="/venus">Venus</Link></li>
+              <li><Link to="/earth">Earth</Link></li>
+              <li><Link to="/mars">Mars</Link></li>
+              <li><Link to="/jupiter">Jupiter</Link></li>
+              <li><Link to="/saturn">Saturn</Link></li>
+              <li><Link to="/uranus">Uranus</Link></li>
+              <li><Link to="/neptune">Neptune</Link></li>
             </ul>
-        </div>
-      </nav>
+          </div>
+        </nav>
+      </header>
     </>
   );
 }
