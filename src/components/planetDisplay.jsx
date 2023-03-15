e694f6033b35c3e27cdd56a928268d3d9a684755
MaxwellChrist <ribthulu@gmail.com>
2023-03-15 15:34:44 -0400

    built out basic styling of planet display component without the data on the bottom and only for larger screen sizes

--- src/components/planetDisplay.jsx
+++ src/components/planetDisplay.jsx
@@ -1,20 +1,45 @@
-import './planetDisplay.css';
-import { useState } from 'react';
-import { ReactComponent  as Picture } from '../assets/planets-fact-site/starter-code/assets/planet-earth.svg'
+import "./planetDisplay.css";
+import { useState } from "react";
+import { ReactComponent as Picture } from "../assets/planets-fact-site/starter-code/assets/planet-earth.svg";
+import { BsFillArrowUpRightSquareFill } from "react-icons/bs";
 
-export default function PlanetDisplay({data}) {
+export default function PlanetDisplay({ data }) {
+  const [planetImage, setPlanetImage] = useState(data.images.planet);
 
-    const [planetImage, setPlanetImage] = useState(data.images.planet)
-
-    return (
-        <>
-            <section>
-                <div className='planet-image-container'>
-                    <h1>{data.name}</h1>
-                    <Picture />
-                    <div className='planet-image' style={{backgroundImage: planetImage}}></div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section>
+        <div className="planet-image-container">
+          <Picture className="planet-image" />
+        </div>
+        <div className="planet-content-container">
+          <h1>{data.name}</h1>
+          <blockquote>
+            <p>{data.overview.content}</p>
+          </blockquote>
+          <figcaption>
+            Source:{" "}
+            <cite>
+              <a href={data.overview.source}>Wikipedia</a>
+              <BsFillArrowUpRightSquareFill id="arrow-icon" />
+            </cite>
+          </figcaption>
+          <div className="button-container">
+          <button className='button-content'>
+                <p className='button-number'>01</p>
+                <p className='button-text'>Overview</p>
+          </button>
+          <button className='button-content'>
+                <p className='button-number'>02</p>
+                <p className='button-text'>Internal Structure</p>
+          </button>
+          <button className='button-content'>
+                <p className='button-number'>03</p>
+                <p className='button-text'>Surface Geology</p>
+          </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
